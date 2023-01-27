423d31a8732fe977bed0412ee9e911680716f8fc
YoussefBenAmmar <benammar.y@gmail.com>
2023-01-27 16:55:35 -0500

    about to add traversal

--- vampire.js
+++ vampire.js
@@ -11,21 +11,42 @@ class Vampire {
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
 
+    this.offspring.push(vampire);
+    vampire.creator = this;
+
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let vampiresFromOriginal = 0;
+    let thisVampire = this;
 
+    while (thisVampire.creator) {
+      thisVampire = thisVampire.creator;
+      vampiresFromOriginal++;
+    }
+
+    return vampiresFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
-  isMoreSeniorThan(vampire) {
 
+  isMoreSeniorThan(vampire) {
+    if (this.offspring === null)
+      return false;
+
+    for (let i = 0; i < this.numberOfOffspring; i++) {
+      if (vampire.name === this.offspring[i].name)
+        return true;
+    }
+
+    return false;
   }
 
   /** Stretch **/
